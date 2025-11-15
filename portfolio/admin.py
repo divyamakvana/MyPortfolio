18eba94f27d1c92d33d5c6464929095a83ae0c39
divya <divyamakavana3004@gmail.com>
2025-11-15 23:38:19 +0530

    upadting some features

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -14,3 +14,6 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('name', 'email', 'message')
+
+
+      
